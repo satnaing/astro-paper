30bf56ccf88673db6fe6b7af9a540c69a66d790e
Sat Naing <satnaingdev@gmail.com>
2022-09-11 16:39:37 +0630

    Create getPostsByTag util function

---
+++ src/utils/getPostsByTag.ts
@@ -0,0 +1,8 @@
+import type { MarkdownInstance } from "astro";
+import { slufigyAll } from "./slugify";
+import type { Frontmatter } from "./types";
+
+const getPostsByTag = (posts: MarkdownInstance<Frontmatter>[], tag: string) =>
+  posts.filter((post) => slufigyAll(post.frontmatter.tags).includes(tag));
+
+export default getPostsByTag;
