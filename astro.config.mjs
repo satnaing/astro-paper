7ca133b529c04702f7bc734ddd8bab84bff0576f
Sat Naing <satnaingdev@gmail.com>
2022-09-12 22:38:51 +0630

    Add site config for canonical url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://astro-paper.netlify.app/",
   integrations: [
     tailwind({
       config: { applyBaseStyles: false },
