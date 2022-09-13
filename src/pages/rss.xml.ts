bc116abb51744257704ea23afbb0d624f2f759fa
Sat Naing <satnaingdev@gmail.com>
2022-09-14 01:46:20 +0630

    Generate rss items with rss feed object

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,24 @@
+import rss from "@astrojs/rss";
+import type { Frontmatter } from "@utils/types";
+import type { MarkdownInstance } from "astro";
+
+const postImportResult = import.meta.glob<MarkdownInstance<Frontmatter>>(
+  "../contents/*.md",
+  {
+    eager: true,
+  }
+);
+const posts = Object.values(postImportResult);
+
+export const get = () =>
+  rss({
+    title: import.meta.env.PUBLIC_SITE_TITLE,
+    description: import.meta.env.PUBLIC_SITE_DESC,
+    site: import.meta.env.SITE,
+    items: posts.map(({ frontmatter }) => ({
+      link: frontmatter.slug,
+      title: frontmatter.title,
+      description: frontmatter.description,
+      pubDate: new Date(frontmatter.datetime),
+    })),
+  });
