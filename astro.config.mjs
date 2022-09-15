98e4e1731044ebd81f3bd212cebabdfb46827b91
Sat Naing <satnaingdev@gmail.com>
2022-09-15 22:00:41 +0630

    Add react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-paper.pages.dev/",
-  integrations: [
-    tailwind({
-      config: { applyBaseStyles: false },
-    }),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), react()]
 });
