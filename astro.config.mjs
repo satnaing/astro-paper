5e534e65b2d748dde396a532e4eb2763bd0fa8ea
Sat Naing <satnaingdev@gmail.com>
2022-09-17 13:56:59 +0630

    Add remark-toc and remark-collapse packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,22 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import remarkToc from "remark-toc";
+import remarkCollapse from "remark-collapse";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-paper.pages.dev/",
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), react()]
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    react(),
+  ],
+  markdown: {
+    remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
+    extendDefaultPlugins: true,
+  },
 });
