b8b7aaef7d7577d107d8679b68c47bd793314e55
Sat Naing <satnaingdev@gmail.com>
2022-09-22 23:36:44 +0630

    Add filter for draft posts

--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -2,10 +2,12 @@ import type { MarkdownInstance } from "astro";
 import type { Frontmatter } from "../types";
 
 const getSortedPosts = (posts: MarkdownInstance<Frontmatter>[]) =>
-  posts.sort(
-    (a, b) =>
-      Math.floor(new Date(b.frontmatter.datetime).getTime() / 1000) -
-      Math.floor(new Date(a.frontmatter.datetime).getTime() / 1000)
-  );
+  posts
+    .filter(({ frontmatter }) => !frontmatter.draft)
+    .sort(
+      (a, b) =>
+        Math.floor(new Date(b.frontmatter.datetime).getTime() / 1000) -
+        Math.floor(new Date(a.frontmatter.datetime).getTime() / 1000)
+    );
 
 export default getSortedPosts;
--- src/utils/getUniqueTags.ts
+++ src/utils/getUniqueTags.ts
@@ -4,7 +4,8 @@ import type { Frontmatter } from "../types";
 
 const getUniqueTags = (posts: MarkdownInstance<Frontmatter>[]) => {
   let tags: string[] = [];
-  posts.forEach((post) => {
+  const filteredPosts = posts.filter(({ frontmatter }) => !frontmatter.draft);
+  filteredPosts.forEach((post) => {
     tags = [...tags, ...post.frontmatter.tags]
       .map((tag) => slugify(tag))
       .filter(
