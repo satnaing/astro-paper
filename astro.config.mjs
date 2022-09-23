fcda70706a9d875cedd41452b16abdca99229573
Sat Naing <satnaingdev@gmail.com>
2022-09-23 21:35:47 +0630

    Update markdown code block theme => one-dark-pro

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,6 +27,10 @@ export default defineConfig({
         },
       ],
     ],
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true,
+    },
     extendDefaultPlugins: true,
   },
 });
