c4c9ab2246ff423698468992222c60b30422beaf
satnaing <satnaingdev@gmail.com>
2022-11-28 21:23:15 +0630

    fix: add optional chaining for theme switch event listener
    Add optional chaining in the event listener of the theme switch button in toggle-theme.js file. This
    is to avoid occuring an error in the console when `light & dark theme mode` is disabled.

--- public/toggle-theme.js
+++ public/toggle-theme.js
@@ -37,7 +37,7 @@ window.onload = () => {
   reflectPreference();
 
   // now this script can find and listen for clicks on the control
-  document.querySelector("#theme-btn").addEventListener("click", () => {
+  document.querySelector("#theme-btn")?.addEventListener("click", () => {
     themeValue = themeValue === "light" ? "dark" : "light";
     setPreference();
   });
