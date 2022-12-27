54d5b1d5ce97d362b46ebfb52e4afc60d949b200
satnaing <satnaingdev@gmail.com>
2022-12-28 00:13:10 +0630

    build: update site option in config file
    Change back to 'https://astro-paper.pages.dev' in site option of astro.config.mjs file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://feat-auto-og-image-generatio.astro-paper.pages.dev/",
+  site: "https://astro-paper.pages.dev/",
   integrations: [
     tailwind({
       config: {
