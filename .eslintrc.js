aaa6dc733be357fa95ca24d8f91b9c780a390591
Sha Mwe La <shamwela2002@gmail.com>
2023-02-02 13:25:04 +0630

    Add parser and fix linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,20 @@ module.exports = {
     es2022: true,
     browser: true,
   },
-  extends: ['eslint:recommended', 'plugin:astro/recommended'],
+  extends: ["eslint:recommended", "plugin:astro/recommended"],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {},
+    },
+  ],
 };
