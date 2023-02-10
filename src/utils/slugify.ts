bcae9856712773887664bb3a3392e1ebfd78607b
John Hooks <bitmachina@outlook.com>
2023-02-10 05:26:55 -0800

    fix: slugifyAll typo

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -6,6 +6,6 @@ export const slugifyStr = (str: string) => slugger(str);
 const slugify = (post: BlogFrontmatter) =>
   post.postSlug ? slugger(post.postSlug) : slugger(post.title);
 
-export const slufigyAll = (arr: string[]) => arr.map(str => slugifyStr(str));
+export const slugifyAll = (arr: string[]) => arr.map(str => slugifyStr(str));
 
 export default slugify;
