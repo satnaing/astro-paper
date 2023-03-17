c9e80feea4fecb0197cde5aa01ac68203e53e7ff
Sat Naing <satnaingdev@gmail.com>
2023-03-17 13:11:02 +0630

    refactor: remove redundant draft filter and update import positions

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,11 +1,11 @@
 import rss from "@astrojs/rss";
-import { getCollection,  } from "astro:content";
+import { getCollection } from "astro:content";
 import getSortedPosts from "@utils/getSortedPosts";
-import { SITE } from "@config";
 import slugify from "@utils/slugify";
+import { SITE } from "@config";
 
 export async function get() {
-  const posts = await getCollection("blog", ({ data }) => !data.draft);
+  const posts = await getCollection("blog");
   const sortedPosts = getSortedPosts(posts);
   return rss({
     title: SITE.title,
