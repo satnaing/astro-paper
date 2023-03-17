9e62b637e8ddb65f5f274fd0154191212dda0590
Salman Hossain Saif <epbd.saif@gmail.com>
2023-03-17 12:44:10 +0600

    fix: sort rss feed from latest to oldest (#38)
    * Sorted the rss feed from latest to oldest. Which is crucial for RSS readers.
    
    * sorted rss using util func
    
    * refactor: remove redundant draft filter and update import positions
    
    ---------
    
    Co-authored-by: Saif71 <>
    Co-authored-by: Sat Naing <satnaingdev@gmail.com>

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,19 +1,22 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { SITE } from "@config";
+import getSortedPosts from "@utils/getSortedPosts";
 import slugify from "@utils/slugify";
+import { SITE } from "@config";
 
 export async function get() {
-  const posts = await getCollection("blog", ({ data }) => !data.draft);
+  const posts = await getCollection("blog");
+  const sortedPosts = getSortedPosts(posts);
   return rss({
     title: SITE.title,
     description: SITE.desc,
     site: SITE.website,
-    items: posts.map(({ data }) => ({
-      link: `posts/${slugify(data)}`,
-      title: data.title,
-      description: data.description,
-      pubDate: new Date(data.pubDatetime),
-    })),
+    items: sortedPosts
+      .map(({ data }) => ({
+        link: `posts/${slugify(data)}`,
+        title: data.title,
+        description: data.description,
+        pubDate: new Date(data.pubDatetime),
+      })),
   });
 }
