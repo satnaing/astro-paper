3b936148b7055da1abe2d645c0f592f0373ac38b
Saif71 <>
2023-03-17 11:45:51 +0600

    sorted rss using util func

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,16 +1,17 @@
 import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
+import { getCollection,  } from "astro:content";
+import getSortedPosts from "@utils/getSortedPosts";
 import { SITE } from "@config";
 import slugify from "@utils/slugify";
 
 export async function get() {
   const posts = await getCollection("blog", ({ data }) => !data.draft);
+  const sortedPosts = getSortedPosts(posts);
   return rss({
     title: SITE.title,
     description: SITE.desc,
     site: SITE.website,
-    items: posts
-      .sort((a, b) => new Date(b.data.pubDatetime).getTime() - new Date(a.data.pubDatetime).getTime())
+    items: sortedPosts
       .map(({ data }) => ({
         link: `posts/${slugify(data)}`,
         title: data.title,
