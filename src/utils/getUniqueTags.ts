7ba158b8757c87d185d386ccddf3a7f499d9b191
Ajit Panigrahi <ajitzero@gmail.com>
2023-06-12 01:27:17 +0530

    refactor: getUniqueTags should perform conversions exactly once per tag
    Previously, the `forEach` loop was spreading the tags list and updating it on every iteration. So for every post, the slugify operation was happening on previously converted data again.
    
    Similarly, the last filter operation to remove duplicates was repeated for every post (`O(N * log N)`) instead of checking once at the end (`O(N)`).

--- src/utils/getUniqueTags.ts
+++ src/utils/getUniqueTags.ts
@@ -2,16 +2,14 @@ import { slugifyStr } from "./slugify";
 import type { CollectionEntry } from "astro:content";
 
 const getUniqueTags = (posts: CollectionEntry<"blog">[]) => {
-  let tags: string[] = [];
   const filteredPosts = posts.filter(({ data }) => !data.draft);
-  filteredPosts.forEach(post => {
-    tags = [...tags, ...post.data.tags]
-      .map(tag => slugifyStr(tag))
-      .filter(
-        (value: string, index: number, self: string[]) =>
-          self.indexOf(value) === index
-      );
-  });
+  const tags: string[] = filteredPosts
+    .flatMap(post => post.data.tags)
+    .map(tag => slugifyStr(tag))
+    .filter(
+      (value: string, index: number, self: string[]) =>
+        self.indexOf(value) === index
+    );
   return tags;
 };
 
