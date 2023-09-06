31969b47be452b931d28d0c385c75a9aad97eea0
Luks17 <lucasthidavort@gmail.com>
2023-09-06 15:13:22 -0400

    chore: fix deprecation and type warnings

--- src/pages/[ogTitle].svg.ts
+++ src/pages/[ogTitle].svg.ts
@@ -2,9 +2,12 @@ import { getCollection } from "astro:content";
 import generateOgImage from "@utils/generateOgImage";
 import type { APIRoute } from "astro";
 
-export const GET: APIRoute = async ({ params }) => ({
-  body: await generateOgImage(params.ogTitle),
-});
+export const GET: APIRoute = async ({ params, ResponseWithEncoding }) =>
+  new ResponseWithEncoding(
+    await generateOgImage(params.ogTitle),
+    undefined,
+    "binary"
+  );
 
 const postImportResult = await getCollection("blog", ({ data }) => !data.draft);
 const posts = Object.values(postImportResult);
