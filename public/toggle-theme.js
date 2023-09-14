cbdaa59baea1c5c5497227dd2fb276e8cf88b936
satnaing <satnaingdev@gmail.com>
2023-09-14 14:00:08 +0630

    feat: add ViewTransitions from Astro
    Integrate ViewTransitions API from Astro v3.
    
    resolve #96

--- public/toggle-theme.js
+++ public/toggle-theme.js
@@ -50,14 +50,21 @@ function reflectPreference() {
 reflectPreference();
 
 window.onload = () => {
-  // set on load so screen readers can get the latest value on the button
-  reflectPreference();
+  function setThemeFeature() {
+    // set on load so screen readers can get the latest value on the button
+    reflectPreference();
 
-  // now this script can find and listen for clicks on the control
-  document.querySelector("#theme-btn")?.addEventListener("click", () => {
-    themeValue = themeValue === "light" ? "dark" : "light";
-    setPreference();
-  });
+    // now this script can find and listen for clicks on the control
+    document.querySelector("#theme-btn")?.addEventListener("click", () => {
+      themeValue = themeValue === "light" ? "dark" : "light";
+      setPreference();
+    });
+  }
+
+  setThemeFeature();
+
+  // Runs on view transitions navigation
+  document.addEventListener("astro:after-swap", setThemeFeature);
 };
 
 // sync with system changes
