b5f54180c8645113ae4e177f3ebb97e1947dc9e2
satnaing <satnaingdev@gmail.com>
2023-09-14 11:53:46 +0630

    feat: update theme-color tag on theme switch

--- public/toggle-theme.js
+++ public/toggle-theme.js
@@ -27,6 +27,23 @@ function reflectPreference() {
   document.firstElementChild.setAttribute("data-theme", themeValue);
 
   document.querySelector("#theme-btn")?.setAttribute("aria-label", themeValue);
+
+  // Get a reference to the body element
+  const body = document.body;
+
+  // Check if the body element exists before using getComputedStyle
+  if (body) {
+    // Get the computed styles for the body element
+    const computedStyles = window.getComputedStyle(body);
+
+    // Get the background color property
+    const bgColor = computedStyles.backgroundColor;
+
+    // Set the background color in <meta theme-color ... />
+    document
+      .querySelector("meta[name='theme-color']")
+      ?.setAttribute("content", bgColor);
+  }
 }
 
 // set early so no page flashes / CSS is made aware
