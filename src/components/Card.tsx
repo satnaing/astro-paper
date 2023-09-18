79fa3d6dd584000d7db32bdcc41fb0c4355b94be
tanishqmanuja <tanishqmanuja@gmail.com>
2023-09-18 13:33:15 +0530

    refactor: handle card styling in component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,13 +11,21 @@ export interface Props {
 export default function Card({ href, frontmatter, secHeading = true }: Props) {
   const { title, pubDatetime, description } = frontmatter;
 
+  const headerProps = {
+    style: { viewTransitionName: slugifyStr(title) },
+    className: "text-lg font-medium decoration-dashed hover:underline",
+  };
+
   return (
-    <li className="list-card">
-      <a href={href}>
+    <li className="list-card my-6">
+      <a
+        href={href}
+        className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+      >
         {secHeading ? (
-          <h2 style={{ viewTransitionName: slugifyStr(title) }}>{title}</h2>
+          <h2 {...headerProps}>{title}</h2>
         ) : (
-          <h3 style={{ viewTransitionName: slugifyStr(title) }}>{title}</h3>
+          <h3 {...headerProps}>{title}</h3>
         )}
       </a>
       <Datetime datetime={pubDatetime} />
