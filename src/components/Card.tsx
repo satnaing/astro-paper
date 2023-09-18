8454f544129d1f27015ed8d5c4a9a073121c1fec
Sat Naing <satnaingdev@gmail.com>
2023-09-18 15:19:51 +0630

    refactor: remove unused class name

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -17,7 +17,7 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
   };
 
   return (
-    <li className="list-card my-6">
+    <li className="my-6">
       <a
         href={href}
         className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
