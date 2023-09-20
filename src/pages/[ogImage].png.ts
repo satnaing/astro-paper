9a7ca46d76852b1b8e9e3971a9d8409e2d62d1e8
tanishqmanuja <tanishqmanuja@gmail.com>
2023-09-20 15:34:47 +0530

    chore: remove unused file

--- src/pages/[ogImage].png.ts
+++
@@ -1,8 +0,0 @@
-import generateOgImages from "@utils/generateOgImages";
-import type { APIRoute } from "astro";
-
-export const GET: APIRoute = async () => new Response(await generateOgImages());
-
-export const getStaticPaths = () => [
-  { params: { ogImage: "generating images with " } },
-];
