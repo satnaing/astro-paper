65e5b918999a475ede005bf8b496a462c10b87a9
tanishqmanuja <tanishqmanuja@gmail.com>
2023-09-20 14:55:29 +0530

    feat: add image validation to schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,18 +2,23 @@ import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
   type: "content",
-  schema: z.object({
-    author: z.string().optional(),
-    pubDatetime: z.date(),
-    title: z.string(),
-    postSlug: z.string().optional(),
-    featured: z.boolean().optional(),
-    draft: z.boolean().optional(),
-    tags: z.array(z.string()).default(["others"]),
-    ogImage: z.string().optional(),
-    description: z.string(),
-    canonicalURL: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      author: z.string().optional(),
+      pubDatetime: z.date(),
+      title: z.string(),
+      postSlug: z.string().optional(),
+      featured: z.boolean().optional(),
+      draft: z.boolean().optional(),
+      tags: z.array(z.string()).default(["others"]),
+      ogImage: image()
+        .refine(img => img.width >= 1200 && img.height >= 630, {
+          message: "OpenGraph image must be at least 1200 X 630 pixels!",
+        })
+        .optional(),
+      description: z.string(),
+      canonicalURL: z.string().optional(),
+    }),
 });
 
 export const collections = { blog };
