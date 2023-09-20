20c89709ada7e7c3f49460d6690d02999ba86d17
tanishqmanuja <tanishqmanuja@gmail.com>
2023-09-20 14:56:50 +0530

    feat: default post author to site author

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,11 @@
+import { SITE } from "@config";
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
   type: "content",
   schema: ({ image }) =>
     z.object({
-      author: z.string().optional(),
+      author: z.string().default(SITE.author),
       pubDatetime: z.date(),
       title: z.string(),
       postSlug: z.string().optional(),
