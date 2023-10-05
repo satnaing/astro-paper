a35e44bb04d01f10b929fe8778384fa1e0f91445
tanishqmanuja <tanishqmanuja@gmail.com>
2023-10-05 13:38:23 +0530

    refactor: single source of truth for site url

--- astro.config.ts
+++ astro.config.ts
@@ -4,10 +4,11 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
+import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  site: SITE.website, // replace this with your deployed domain
   integrations: [
     tailwind({
       applyBaseStyles: false,
