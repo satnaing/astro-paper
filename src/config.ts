989598310ef0c3595dec100129288e81eb6c57ff
Moritz Frisch <mo_68@gmx.de>
2023-11-04 17:24:20 +0100

    fav icons

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,22 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  website: "https://rhodos-weiterstadt.de/", // replace this with your deployed domain
+  author: "Moritz Frisch",
+  desc: "Dies ist die Seite des Restaurants Rhodos in Weiterstadt.",
+  title: "Restaurant Rhodos",
+  ogImage: "",
   lightAndDarkMode: true,
   postPerPage: 3,
 };
 
-export const LOCALE = ["en-EN"]; // set to [] to use the environment default
+export const LOCALE = ["de-DE"]; // set to [] to use the environment default
 
 export const LOGO_IMAGE = {
-  enable: false,
-  svg: true,
-  width: 216,
-  height: 46,
+  enable: true,
+  svg: false,
+  width: 48,
+  height: 48,
 };
 
 export const SOCIALS: SocialObjects = [
