dbd7fb2e4c10cdf02676a8ac647d28afbea34141
Moritz Frisch <mo_68@gmx.de>
2023-11-06 16:37:51 +0100

    check again

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,6 @@ function withOpacity(variableName) {
   };
 }
 
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
