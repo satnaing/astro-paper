db31df3ce4bb961b131d836ce7bb530191d5ace0
satnaing <satnaingdev@gmail.com>
2024-01-09 11:06:09 +0630

    refactor: remove SocialObjects type and update SocialObjects type
    - remove SocialMedia type from src/types.ts
    - generate each social icon type from socialIcons object instead

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import type socialIcons from "@assets/socialIcons";
+
 export type Site = {
   website: string;
   author: string;
@@ -9,34 +11,8 @@ export type Site = {
 };
 
 export type SocialObjects = {
-  name: SocialMedia;
+  name: keyof typeof socialIcons;
   href: string;
   active: boolean;
   linkTitle: string;
 }[];
-
-export type SocialIcons = {
-  [social in SocialMedia]: string;
-};
-
-export type SocialMedia =
-  | "Github"
-  | "Facebook"
-  | "Instagram"
-  | "LinkedIn"
-  | "Mail"
-  | "Twitter"
-  | "Twitch"
-  | "YouTube"
-  | "WhatsApp"
-  | "Snapchat"
-  | "Pinterest"
-  | "TikTok"
-  | "CodePen"
-  | "Discord"
-  | "GitLab"
-  | "Reddit"
-  | "Skype"
-  | "Steam"
-  | "Telegram"
-  | "Mastodon";
