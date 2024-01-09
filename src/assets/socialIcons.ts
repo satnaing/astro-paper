282b0c5e5886a06a19ae7ed0e145f85fd4793d4d
satnaing <satnaingdev@gmail.com>
2024-01-09 11:06:09 +0630

    refactor: remove SocialObjects type and update SocialObjects type
    - remove SocialMedia type from src/types.ts
    - generate each social icon type from socialIcons object instead

--- src/assets/socialIcons.ts
+++ src/assets/socialIcons.ts
@@ -1,6 +1,4 @@
-import type { SocialIcons } from "../types";
-
-const socialIcons: SocialIcons = {
+const socialIcons = {
   Github: `<svg
     xmlns="http://www.w3.org/2000/svg"
     class="icon-tabler"
