7e74e3eec26af3b0c5a8e8ca8e70fcc4d8138dc7
FjellOverflow <fjelloverflow@protonmail.com>
2024-01-18 15:40:24 +0200

    feat: syntax-highlighting dark/light themed according to site dark/light theme

--- astro.config.ts
+++ astro.config.ts
@@ -27,7 +27,10 @@ export default defineConfig({
       ],
     ],
     shikiConfig: {
-      theme: "one-dark-pro",
+      experimentalThemes: {
+        dark: "one-dark-pro",
+        light: "solarized-light",
+      },
       wrap: true,
     },
   },
