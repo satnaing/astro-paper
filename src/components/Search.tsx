b3a1d9d369fe76f13bbe379691d99fd0a40c892b
面条 <2959393+ccbikai@users.noreply.github.com>
2024-01-25 11:54:28 +0800

    fix: add trailing slash to links to avoid extra redirects (#246)
    Add trailing slash to links for improved consistency with RSS links and to avoid unnecessary redirects.
    
    ---------
    
    Co-authored-by: satnaing <satnaingdev@gmail.com>

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -112,7 +112,7 @@ export default function SearchBar({ searchList }: Props) {
         {searchResults &&
           searchResults.map(({ item, refIndex }) => (
             <Card
-              href={`/posts/${item.slug}`}
+              href={`/posts/${item.slug}/`}
               frontmatter={item.data}
               key={`${refIndex}-${item.slug}`}
             />
