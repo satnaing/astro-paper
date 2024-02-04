2c7dbd5c01dabb5bb7d1477e71d725be0b3328b2
David Legrand <1110600+davlgd@users.noreply.github.com>
2024-02-05 00:03:24 +0100

    feat: add JSON-LD Structured data
    - Add author profile to SITE variables
    - Add JSON-LD to Layout.astro

--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,7 @@ import type socialIcons from "@assets/socialIcons";
 export type Site = {
   website: string;
   author: string;
+  profile: string;
   desc: string;
   title: string;
   ogImage?: string;
