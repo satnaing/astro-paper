11286e0c6b55d2d406d50852ad8f7c7335c5731d
rachel cantor <1870406+rachelslurs@users.noreply.github.com>
2024-07-02 14:09:14 -0400

    fix: adding data-theme to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,7 @@ function withOpacity(variableName) {
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["selector", "[data-theme='dark']"],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     // Remove the following screen breakpoint or add other breakpoints
