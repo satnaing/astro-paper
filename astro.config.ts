4a27cc1e8a1465f46658a10e21fd38ce33344a23
satnaing <satnaingdev@gmail.com>
2024-07-20 14:45:57 +0700

    feat: support light/dark theme in code blocks
    Update Astro and its related dependencies. Support
    light/dark theme in code-blocks.

--- astro.config.ts
+++ astro.config.ts
@@ -27,7 +27,8 @@ export default defineConfig({
       ],
     ],
     shikiConfig: {
-      theme: "one-dark-pro",
+      // For more themes, visit https://shiki.style/themes
+      themes: { light: "min-light", dark: "night-owl" },
       wrap: true,
     },
   },
