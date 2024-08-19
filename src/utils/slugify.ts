2b617b20a371a333b71ac7afec534c2d29e96534
Alejandro Ñáñez Ortiz <git@alejandro.dev>
2024-08-19 00:52:59 -0500

    refactor: replace github-slugger with lodash.kebabcase (#357)
    Previously, `github-slugger` omitted emojis when generating slugs, transforming tags like "🌱-my-tag" into "-my-tag". This change replaces `github-slugger` with `lodash.kebabcase` to ensure that emojis are preserved in the generated slugs, improving the accuracy and consistency of tag representations.

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,5 +1,5 @@
-import { slug as slugger } from "github-slugger";
+import kebabcase from "lodash.kebabcase";
 
-export const slugifyStr = (str: string) => slugger(str);
+export const slugifyStr = (str: string) => kebabcase(str);
 
 export const slugifyAll = (arr: string[]) => arr.map(str => slugifyStr(str));
