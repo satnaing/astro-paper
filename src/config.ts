dfc3f0a668708c1de5386aeb1df83eee49be5af4
Wilson <scorpioaoe@gmail.com>
2024-09-22 14:11:50 +0800

    feat: add archives page with configurable menu

--- src/config.ts
+++ src/config.ts
@@ -11,6 +11,7 @@ export const SITE: Site = {
   postPerIndex: 4,
   postPerPage: 3,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  showArchives: true,
   editPost: {
     url: "https://github.com/satnaing/astro-paper/edit/main/src/content/blog",
     text: "Suggest Changes",
--- src/types.ts
+++ src/types.ts
@@ -11,6 +11,7 @@ export type Site = {
   postPerIndex: number;
   postPerPage: number;
   scheduledPostMargin: number;
+  showArchives?: boolean;
   editPost?: {
     url?: URL["href"];
     text?: string;
