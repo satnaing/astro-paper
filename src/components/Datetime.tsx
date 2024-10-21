537985e698dfe340b1b2262c9ffb0cfdff5733f3
Erison Silva <hey@erison.work>
2024-10-21 11:21:09 +0200

    feat: Add pencil icon before suggestion changes text

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -102,7 +102,17 @@ const EditPost = ({ editPost, postId }: EditPostProps) => {
           rel="noopener noreferrer"
           target="_blank"
         >
-          {editPostText}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="scale-90 inline-block h-6 w-6 min-w-[1.375rem] fill-skin-base icon icon-tabler icons-tabler-outline icon-tabler-edit"
+            aria-hidden="true"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
+            <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
+            <path d="M16 5l3 3" />
+          </svg>
+          <span class="ml-2 italic text-base">{editPostText}</span>
         </a>
       </>
     )
