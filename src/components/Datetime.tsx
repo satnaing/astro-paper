6091a8d340b0c5030c1c577e62e8925f5276a5cc
satnaing <satnaingdev@gmail.com>
2024-10-29 12:57:30 +0700

    fix: update class to className and adjust icon scale

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -97,14 +97,14 @@ const EditPost = ({ editPost, postId }: EditPostProps) => {
       <>
         <span aria-hidden="true"> | </span>
         <a
-          className="hover:opacity-75"
+          className="space-x-1.5 hover:opacity-75"
           href={editPostUrl}
           rel="noopener noreferrer"
           target="_blank"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="scale-90 inline-block h-6 w-6 min-w-[1.375rem] fill-skin-base icon icon-tabler icons-tabler-outline icon-tabler-edit"
+            className="icon icon-tabler icons-tabler-outline icon-tabler-edit inline-block !scale-90 fill-skin-base"
             aria-hidden="true"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -112,7 +112,7 @@ const EditPost = ({ editPost, postId }: EditPostProps) => {
             <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
             <path d="M16 5l3 3" />
           </svg>
-          <span class="ml-2 italic text-base">{editPostText}</span>
+          <span className="text-base italic">{editPostText}</span>
         </a>
       </>
     )
