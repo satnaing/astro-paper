63fe796eace411404ebf5ef5d55b8b90172b736e
Sébastien Monterisi <sebastienmonterisi@yahoo.fr>
2024-12-29 10:04:20 +0100

    fix: focus search input on mount (#414)

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -73,6 +73,13 @@ export default function SearchBar({ searchList }: Props) {
     }
   }, [inputVal]);
 
+  useEffect(() => {
+    // focus on text input when search bar is displayed
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [inputVal]);
+
   return (
     <>
       <label className="relative block">
