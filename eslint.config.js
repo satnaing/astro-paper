846315a265757f345761762b59868184c03a8806
satnaing <satnaingdev@gmail.com>
2025-02-25 21:18:41 +0700

    chore: remove eslint-plugin-jsx-a11y package

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,8 @@
 import eslintPluginAstro from "eslint-plugin-astro";
 import globals from "globals";
-import jsxA11y from "eslint-plugin-jsx-a11y";
 import tseslint from "typescript-eslint";
 
 export default [
-  jsxA11y.flatConfigs.recommended,
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {
