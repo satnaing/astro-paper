fa0bcddee338697b51e488fed854df76f8a5d3c6
satnaing <satnaingdev@gmail.com>
2025-02-25 20:35:00 +0700

    build!: upgrade to Tailwind CSS v4

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -1,56 +1,49 @@
-import { LOCALE, SITE } from "@config";
-import type { CollectionEntry } from "astro:content";
+import { LOCALE } from "@/constants";
 
 interface DatetimesProps {
   pubDatetime: string | Date;
   modDatetime: string | Date | undefined | null;
 }
 
-interface EditPostProps {
-  editPost?: CollectionEntry<"blog">["data"]["editPost"];
-  postId?: CollectionEntry<"blog">["id"];
-}
-
-interface Props extends DatetimesProps, EditPostProps {
+interface Props extends DatetimesProps {
   size?: "sm" | "lg";
   className?: string;
 }
 
-export default function Datetime({
+export function Datetime({
   pubDatetime,
   modDatetime,
   size = "sm",
   className = "",
-  editPost,
-  postId,
 }: Props) {
   return (
-    <div
-      className={`flex items-center space-x-2 opacity-80 ${className}`.trim()}
-    >
+    <div className={`flex items-end space-x-2 opacity-80 ${className}`.trim()}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className={`${
           size === "sm" ? "scale-90" : "scale-100"
-        } inline-block h-6 w-6 min-w-[1.375rem] fill-skin-base`}
+        } inline-block h-6 w-6 min-w-[1.375rem] fill-foreground`}
         aria-hidden="true"
       >
         <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
         <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path>
       </svg>
       {modDatetime && modDatetime > pubDatetime ? (
-        <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
+        <span
+          className={`italic ${size === "sm" ? "text-sm" : "text-sm sm:text-base"}`}
+        >
           Updated:
         </span>
       ) : (
         <span className="sr-only">Published:</span>
       )}
-      <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
+      <span
+        className={`italic ${size === "sm" ? "text-sm" : "text-sm sm:text-base"}`}
+      >
         <FormattedDatetime
           pubDatetime={pubDatetime}
           modDatetime={modDatetime}
         />
-        {size === "lg" && <EditPost editPost={editPost} postId={postId} />}
       </span>
     </div>
   );
@@ -79,42 +72,5 @@ const FormattedDatetime = ({ pubDatetime, modDatetime }: DatetimesProps) => {
       <span className="sr-only">&nbsp;at&nbsp;</span>
       <span className="text-nowrap">{time}</span>
     </>
-  );
-};
-
-const EditPost = ({ editPost, postId }: EditPostProps) => {
-  let editPostUrl = editPost?.url ?? SITE?.editPost?.url ?? "";
-  const showEditPost = !editPost?.disabled && editPostUrl.length > 0;
-  const appendFilePath =
-    editPost?.appendFilePath ?? SITE?.editPost?.appendFilePath ?? false;
-  if (appendFilePath && postId) {
-    editPostUrl += `/${postId}`;
-  }
-  const editPostText = editPost?.text ?? SITE?.editPost?.text ?? "Edit";
-
-  return (
-    showEditPost && (
-      <>
-        <span aria-hidden="true"> | </span>
-        <a
-          className="space-x-1.5 hover:opacity-75"
-          href={editPostUrl}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="icon icon-tabler icons-tabler-outline icon-tabler-edit inline-block !scale-90 fill-skin-base"
-            aria-hidden="true"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
-            <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
-            <path d="M16 5l3 3" />
-          </svg>
-          <span className="text-base italic">{editPostText}</span>
-        </a>
-      </>
-    )
   );
 };
