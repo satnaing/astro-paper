f616c52b208acecf3c23f41dd44a4624a3b04e8d
satnaing <satnaingdev@gmail.com>
2025-02-25 20:20:43 +0700

    refactor!: separate config and constants

---
+++ src/constants.ts
@@ -0,0 +1,80 @@
+import IconMail from "@/assets/icons/IconMail.svg";
+import IconGitHub from "@/assets/icons/IconGitHub.svg";
+import IconBrandX from "@/assets/icons/IconBrandX.svg";
+import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
+import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
+import IconFacebook from "@/assets/icons/IconFacebook.svg";
+import IconTelegram from "@/assets/icons/IconTelegram.svg";
+import IconPinterest from "@/assets/icons/IconPinterest.svg";
+import { SITE } from "@/config";
+
+export const LOCALE = {
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+} as const;
+
+export const SOCIALS = [
+  {
+    name: "Github",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: ` ${SITE.title} on Github`,
+    icon: IconGitHub,
+  },
+  {
+    name: "X",
+    href: "https://x.com/username",
+    linkTitle: `${SITE.title} on X`,
+    icon: IconBrandX,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/username/",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    icon: IconLinkedin,
+  },
+  {
+    name: "Mail",
+    href: "mailto:yourmail@gmail.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    icon: IconMail,
+  },
+] as const;
+
+export const SHARE_LINKS = [
+  {
+    name: "WhatsApp",
+    href: "https://wa.me/?text=",
+    linkTitle: `Share this post via WhatsApp`,
+    icon: IconWhatsapp,
+  },
+  {
+    name: "Facebook",
+    href: "https://www.facebook.com/sharer.php?u=",
+    linkTitle: `Share this post on Facebook`,
+    icon: IconFacebook,
+  },
+  {
+    name: "X",
+    href: "https://x.com/intent/post?url=",
+    linkTitle: `Share this post on X`,
+    icon: IconBrandX,
+  },
+  {
+    name: "Telegram",
+    href: "https://t.me/share/url?url=",
+    linkTitle: `Share this post via Telegram`,
+    icon: IconTelegram,
+  },
+  {
+    name: "Pinterest",
+    href: "https://pinterest.com/pin/create/button/?url=",
+    linkTitle: `Share this post on Pinterest`,
+    icon: IconPinterest,
+  },
+  {
+    name: "Mail",
+    href: "mailto:?subject=See%20this%20post&body=",
+    linkTitle: `Share this post via email`,
+    icon: IconMail,
+  },
+] as const;
