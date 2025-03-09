212fd5c550a2b3765a34058a57b28eadd7104ad9
Sat Naing <satnaingdev@gmail.com>
2025-03-09 19:27:20 +0700

    fix: remove unused `ogImage` size validation (#462)
    * fix: remove unused ogImage size validation
    
    Validation of `ogImage` field in blog collection
    causes unintentional broken og images after the build.
    Removing the validation solve the issue.
    
    * docs: update docs for ogImage path
    
    * chore: update asset

--- src/content.config.ts
+++ src/content.config.ts
@@ -13,12 +13,7 @@ const blog = defineCollection({
       featured: z.boolean().optional(),
       draft: z.boolean().optional(),
       tags: z.array(z.string()).default(["others"]),
-      ogImage: image()
-        .refine(img => img.width >= 1200 && img.height >= 630, {
-          message: "OpenGraph image must be at least 1200 X 630 pixels!",
-        })
-        .or(z.string())
-        .optional(),
+      ogImage: image().or(z.string()).optional(),
       description: z.string(),
       canonicalURL: z.string().optional(),
       editPost: z
