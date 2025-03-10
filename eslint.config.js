09efddbedbf5e783c08cfb17b8662f1c61f4fb7e
Alexey Poimtsev <25312+alec-c4@users.noreply.github.com>
2025-03-10 14:27:22 +0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -14,5 +14,5 @@ export default [
     },
   },
   { rules: { "no-console": "error" } },
-  { ignores: ["dist/**", ".astro", "public/pagefind/**"] },
+  { ignores: ["dist/**", "**/*.astro", "public/pagefind/**"] },
 ];
