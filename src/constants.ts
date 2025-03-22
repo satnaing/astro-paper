c5ea9b185bd946224f7347787e75529e25cd8f9b
satnaing <satnaingdev@gmail.com>
2025-03-22 16:15:06 +0700

    refactor: replace `LOCALE` constant with  `SITE.lang`

--- src/constants.ts
+++ src/constants.ts
@@ -8,11 +8,6 @@ import IconTelegram from "@/assets/icons/IconTelegram.svg";
 import IconPinterest from "@/assets/icons/IconPinterest.svg";
 import { SITE } from "@/config";
 
-export const LOCALE = {
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
-} as const;
-
 export const SOCIALS = [
   {
     name: "Github",
