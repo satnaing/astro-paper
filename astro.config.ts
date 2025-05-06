c09919b17ec4b5ae9b843ae4c95063dba19c19d8
David Legrand <1110600+davlgd@users.noreply.github.com>
2025-05-06 14:20:20 +0200

    fix: experimental features
    Fix #514

--- astro.config.ts
+++ astro.config.ts
@@ -30,10 +30,9 @@ export default defineConfig({
   image: {
     // Used for all Markdown images; not configurable per-image
     // Used for all `<Image />` and `<Picture />` components unless overridden with a prop
-    experimentalLayout: "responsive",
+    experimentalLayout: "constrained",
   },
   experimental: {
-    svg: true,
     responsiveImages: true,
     preserveScriptOrder: true,
   },
