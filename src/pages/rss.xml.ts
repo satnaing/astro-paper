dcabc1f42e6b96170b139539a806a9e2260bc47e
achuanya <haibao1027@gmail.com>
2025-07-02 00:45:42 +0800

    Feed Ownership Verification

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -11,11 +11,19 @@ export async function GET() {
     title: SITE.title,
     description: SITE.desc,
     site: SITE.website,
-    items: sortedPosts.map(({ data, id, filePath }) => ({
-      link: getPath(id, filePath),
-      title: data.title,
-      description: data.description,
-      pubDate: new Date(data.modDatetime ?? data.pubDatetime),
-    })),
+    items: [
+      {
+        link: SITE.website,
+        title: 'Feed Ownership Verification',
+        description: 'This message is used to verify that this feed (feedId:55149012216215602) belongs to me (userId:109876092687369216). Join me in enjoying the next generation information browser https://follow.is.',
+        pubDate: new Date(),
+      },
+      ...sortedPosts.map(({ data, id, filePath }) => ({
+        link: getPath(id, filePath),
+        title: data.title,
+        description: data.description,
+        pubDate: new Date(data.modDatetime ?? data.pubDatetime),
+      })),
+    ],
   });
 }
