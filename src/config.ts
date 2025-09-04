21234da36818e0e34e781880fe2e95e445639ece
展堂 <zhantang@qunhemail.com>
2025-09-04 18:47:29 +0800

    feat(font): 修改默认字体

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,23 @@
 export const SITE = {
   website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
   author: "Sat Naing",
-  profile: "https://satnaing.dev/",
+  profile: "https://astro-paper.pages.dev/about",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "Doit's Blog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
-  postPerPage: 4,
+  postPerPage: 8,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    enabled: true,
+    enabled: false,
     text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  lang: "zh-CN", // html lang code. Set this empty and default will be "en"
+  timezone: "Asia/Shanghai", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
--- src/constants.ts
+++ src/constants.ts
@@ -3,10 +3,10 @@ import IconMail from "@/assets/icons/IconMail.svg";
 import IconGitHub from "@/assets/icons/IconGitHub.svg";
 import IconBrandX from "@/assets/icons/IconBrandX.svg";
 import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
-import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
-import IconFacebook from "@/assets/icons/IconFacebook.svg";
-import IconTelegram from "@/assets/icons/IconTelegram.svg";
-import IconPinterest from "@/assets/icons/IconPinterest.svg";
+// import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
+// import IconFacebook from "@/assets/icons/IconFacebook.svg";
+// import IconTelegram from "@/assets/icons/IconTelegram.svg";
+// import IconPinterest from "@/assets/icons/IconPinterest.svg";
 import { SITE } from "@/config";
 
 interface Social {
@@ -44,40 +44,40 @@ export const SOCIALS: Social[] = [
 ] as const;
 
 export const SHARE_LINKS: Social[] = [
-  {
-    name: "WhatsApp",
-    href: "https://wa.me/?text=",
-    linkTitle: `Share this post via WhatsApp`,
-    icon: IconWhatsapp,
-  },
-  {
-    name: "Facebook",
-    href: "https://www.facebook.com/sharer.php?u=",
-    linkTitle: `Share this post on Facebook`,
-    icon: IconFacebook,
-  },
-  {
-    name: "X",
-    href: "https://x.com/intent/post?url=",
-    linkTitle: `Share this post on X`,
-    icon: IconBrandX,
-  },
-  {
-    name: "Telegram",
-    href: "https://t.me/share/url?url=",
-    linkTitle: `Share this post via Telegram`,
-    icon: IconTelegram,
-  },
-  {
-    name: "Pinterest",
-    href: "https://pinterest.com/pin/create/button/?url=",
-    linkTitle: `Share this post on Pinterest`,
-    icon: IconPinterest,
-  },
-  {
-    name: "Mail",
-    href: "mailto:?subject=See%20this%20post&body=",
-    linkTitle: `Share this post via email`,
-    icon: IconMail,
-  },
+  // {
+  //   name: "WhatsApp",
+  //   href: "https://wa.me/?text=",
+  //   linkTitle: `Share this post via WhatsApp`,
+  //   icon: IconWhatsapp,
+  // },
+  // {
+  //   name: "Facebook",
+  //   href: "https://www.facebook.com/sharer.php?u=",
+  //   linkTitle: `Share this post on Facebook`,
+  //   icon: IconFacebook,
+  // },
+  // {
+  //   name: "X",
+  //   href: "https://x.com/intent/post?url=",
+  //   linkTitle: `Share this post on X`,
+  //   icon: IconBrandX,
+  // },
+  // {
+  //   name: "Telegram",
+  //   href: "https://t.me/share/url?url=",
+  //   linkTitle: `Share this post via Telegram`,
+  //   icon: IconTelegram,
+  // },
+  // {
+  //   name: "Pinterest",
+  //   href: "https://pinterest.com/pin/create/button/?url=",
+  //   linkTitle: `Share this post on Pinterest`,
+  //   icon: IconPinterest,
+  // },
+  // {
+  //   name: "Mail",
+  //   href: "mailto:?subject=See%20this%20post&body=",
+  //   linkTitle: `Share this post via email`,
+  //   icon: IconMail,
+  // },
 ] as const;
