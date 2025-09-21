9b79c61df9ed529ccddc8dd23bcdd7cb9f36eeb1
展堂 <zhantang@qunhemail.com>
2025-09-21 15:50:03 +0800

    chore(build): 修改构建配置

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,15 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://blog.doooit.me/", // replace this with your deployed domain
   author: "Doit",
-  profile: "https://astro-paper.pages.dev/about",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  profile: "https://blog.doooit.me/about",
+  desc: "Doit杜伊特的个人博客，分享自己生活日常和技术探索，记录所思、所见、所想。",
   title: "Doit的博客",
-  ogImage: "astropaper-og.jpg",
+  ogImage: "doit-og.png",
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 8,
-  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  // scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  scheduledPostMargin: 0, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
