fe8e0a721b7216ff87fe2dfd5ba3251a410dd0f3
prithvikrishnab4u <prithvikrishnab4u@gmail.com>
2025-10-09 13:08:06 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,6 @@ export const SITE = {
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
-  showBackButton: true,
   editPost: {
   enabled: true,
   text: "Edit page",
