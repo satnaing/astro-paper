47acd427a627759530e2b54dc661f2acbf418df6
prithvikrishnab4u <prithvikrishnab4u@gmail.com>
2025-10-09 12:00:09 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,24 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  website: "https://pporeddy.com", // your domain
+  author: "Prithvi Poreddy",
+  profile: "https://www.linkedin.com/in/pporeddy/, // optional
+  desc: "Writing about Identity security, Cyber Security, AI Governace & Security and Personal Finace.",
+  title: "Prithvi Poreddy",
+  ogImage: "astropaper-og.jpg", // leave default
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 4,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
+  showBackButton: true,
   editPost: {
-    enabled: true,
-    text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
-  },
+  enabled: true,
+  text: "Edit page",
+  url: "https://github.com/prithviporeddy/pporeddy-site/edit/main/",
+},
+  dir: "ltr",
+  lang: "en",
+  timezone: "America/Los_Angeles", // optional but correct for you
   dynamicOgImage: true,
-  dir: "ltr", // "rtl" | "auto"
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
